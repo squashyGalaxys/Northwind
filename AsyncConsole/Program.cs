a87c46ee9ef255f11c4835f0ea7201216e4c208d
Nevena Kicanovic <nevena@contactus.se>
2025-01-03 11:53:24 +0100

    L2025-01-03

---
+++ AsyncConsole/Program.cs
@@ -0,0 +1,7 @@
+﻿HttpClient client = new HttpClient();
+
+HttpResponseMessage response =
+    await client.GetAsync("http://www.apple.com");
+
+
+WriteLine($"Apple's home page has {response.Content.Headers.ContentLength} bytes");
---
+++ Debbugging/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+
+string settingsFile = "appsettings.json";
+
+string settingsPath = Path.Combine(Directory.GetCurrentDirectory(), settingsFile);
+
+Console.WriteLine($"Processing...: {settingsPath}");
+Console.WriteLine(File.ReadAllText(settingsPath));
+Console.WriteLine("--------------------");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+builder.AddJsonFile(settingsFile, 
+    optional: false, reloadOnChange: true);
+
+IConfigurationRoot configuration = builder.Build();
+
+TraceSwitch ts = new(
+    displayName: "NinSwitch",
+    description: "This switch is set via a JSON config.");
+
+configuration.GetSection("NinSwitch").Bind(ts);
+
+
+Console.WriteLine($"TraceSwitch value: {ts.Value}");
+Console.WriteLine($"TraceSwitch level: {ts.Level}");
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace info");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
+
+Debug.Close();
+Trace.Close();
+
+Console.WriteLine("Done. Press enter to exit");
+Console.ReadLine();
+
+
+
+
