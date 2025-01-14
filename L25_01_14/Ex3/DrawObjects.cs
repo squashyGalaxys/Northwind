c2af68c87726f04fd74474f03172d6e5874438c4
Nevena Kicanovic <nevena@contactus.se>
2025-01-14 11:59:49 +0100

    Ex3_25_01_14

---
+++ L25_01_14/Ex3/DrawObjects.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    public class DrawObject
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Jag är bas ritobjekt.");
+        }
+    }
+    public class Line : DrawObject
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Jag är en linje.");
+        }
+    }
+    public class Circle : DrawObject
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Jag är en cirkel.");
+        }
+    }
+    public class Square : DrawObject
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Jag är en kvadrat.");
+        }
+    }
+}
---
+++ L25_01_14/Ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Skriv en konsolapplikation för att skapa följande klasser:
+//•	DrawObject som innehåller en virtuell metod Draw() som skriver ut texten ”Jag är bas ritobjekt.",
+//•	Klass Line som ärver klassen DrawObject och har metoden Draw()
+//•	Circle som ärver DrawObject-klassen och har en Draw()-metod,
+//•	Kvadrat som ärver klassen DrawObject och har metoden Draw()
+//•	DemoForDrawing (där Main /program.cs) - funktionen definieras med skapade objekt för ritning
+
+using Ex3;
+
+DrawObject[] drawObjects = new DrawObject[4];
+drawObjects[0] = new Line();
+drawObjects[1] = new Circle();
+drawObjects[2] = new Square();
+drawObjects[3] = new DrawObject();
+
+foreach (DrawObject drawObject in drawObjects)
+{
+    drawObject.Draw();
+}
+
+Console.ReadKey();
