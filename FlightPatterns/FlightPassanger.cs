57c38160463dab338285368c9a8f3bfa18701e91
Nevena Kicanovic <nevena@contactus.se>
2025-01-16 09:24:36 +0100

    FlightPassengers_Klasser

---
+++ FlightPatterns/FlightPassanger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightPatterns
+{
+    public class Passanger
+    {
+        public string? Name { get; set; }
+
+    }
+
+    public class BusinessClassPassanger : Passanger
+    {
+        public override string ToString()
+        {
+            return $"Business Class Passanger: {Name}";
+        }
+    }
+
+    public class  CoachClassPassanger : Passanger
+    {
+        public double CarryOnKG { get; set; }
+        public override string ToString()
+        {
+            return $"Coach Class Passanger {Name} with {CarryOnKG} KG carry on ";
+        }
+    }
+
+    public class FirstClassPassanger : Passanger
+    {
+        public int AirMiles { get; set; }
+        public override string ToString()
+        {
+            return $"First Class Passanger {Name} with {AirMiles} air miles";
+        }
+    }
+}
