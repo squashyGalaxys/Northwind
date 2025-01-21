eb00b2ecb05cfbea565ebb3b046a217420891584
Nevena Kicanovic <nevena@contactus.se>
2025-01-21 10:11:02 +0100

    Delegate_25_01_21

---
+++ DelegatesEx/Program.cs
@@ -0,0 +1,15 @@
+﻿using DelegatesEx;
+
+static void DisplayMessage(string message)
+{
+    Console.WriteLine($"Message: {message}");
+}
+// Create an instance of the delegate and assign the method DisplayMessage
+SimpleDelegate delegateInstance = DisplayMessage;
+
+// Invoke the method through delegate
+delegateInstance("Hello, Delegates!");
+
+SimpleDelegate delegateInstance2 = DisplayMessage;
+
+delegateInstance2("Another message");
