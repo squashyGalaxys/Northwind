2a2c396ddc91a66fe8c40b179ff56cb280005864
Nevena Kicanovic <nevena@contactus.se>
2025-01-30 09:52:09 +0100

    V1.1

--- TicketBooking/TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
+++ TicketBooking/TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -1,11 +1,15 @@
+using Moq;
+
 namespace TicketBookingCore.Tests
 {
     public class TicketBookingRequestProcessorTests
     {
+        private readonly Mock<ITicketBookingRepository> _ticketBookingRepositoryMock;
         private readonly TicketBookingRequestProcessor _processor;
         public TicketBookingRequestProcessorTests()
         {
-            _processor = new TicketBookingRequestProcessor();
+            _ticketBookingRepositoryMock = new Mock<ITicketBookingRepository>();
+            _processor = new TicketBookingRequestProcessor(_ticketBookingRepositoryMock.Object);
         }
 
         [Fact]
@@ -38,5 +42,38 @@ namespace TicketBookingCore.Tests
             //Assert
             Assert.Equal("request", exception.ParamName);
         }
+
+        /// <summary>
+        /// This test will fail because the method is not implemented yet.
+        /// </summary>
+        [Fact]
+        public void ShouldSaveToDatabase()
+        {
+            // Arrange
+            TicketBooking savedTicketBooking = null;
+
+            // Setup the Save method to capture the saved ticket booking
+            _ticketBookingRepositoryMock.Setup(x => x.Save(It.IsAny<TicketBooking>()))
+            .Callback<TicketBooking>((ticketBooking) =>
+            {
+                savedTicketBooking = ticketBooking;
+            });
+
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Milena",
+                LastName = "Avramovic",
+                Email = "milenaavramovic@gmail.com"
+            };
+
+            // Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            // Assert
+            Assert.NotNull(savedTicketBooking);
+            Assert.Equal(request.FirstName, savedTicketBooking.FirstName);
+            Assert.Equal(request.LastName, savedTicketBooking.LastName);
+            Assert.Equal(request.Email, savedTicketBooking.Email);
+        }   
     }
 }
--- TicketBooking/TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBooking/TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -3,7 +3,7 @@ namespace TicketBookingCore
 {
     public class TicketBookingRequestProcessor
     {
-        public TicketBookingRequestProcessor()
+        public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
         }
 
