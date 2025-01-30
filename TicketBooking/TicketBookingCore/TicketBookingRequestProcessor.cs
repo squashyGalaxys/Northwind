fc8f0e29d2fc8e3a513f1c9f1966bfd863f0e983
Nevena Kicanovic <nevena@contactus.se>
2025-01-30 11:06:37 +0100

    v1.2, Skriva kod för att få testet att passera

--- TicketBooking/TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBooking/TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -3,8 +3,11 @@ namespace TicketBookingCore
 {
     public class TicketBookingRequestProcessor
     {
+        //suggesting that _ticketBookingRepository will hold a reference to an object that implements this interface.
+        private readonly ITicketBookingRepository _ticketBookingRepository;
         public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
+            _ticketBookingRepository = ticketBookingRepository;
         }
 
         public TicketBookingResponse Book(TicketBookingRequest request)
@@ -13,6 +16,15 @@ namespace TicketBookingCore
             {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            //kod för att spara i databasen
+            _ticketBookingRepository.Save(new TicketBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email
+            });
+
             //refractor för att returnera en ny TicketBookingResponse
             return new TicketBookingResponse
             {
