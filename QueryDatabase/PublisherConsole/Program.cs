87655fb5482f10d79abc0103e38a57dc787aa045
Nevena Kicanovic <nevena@contactus.se>
2025-02-25 13:56:49 +0100

    25 feb 13:56

--- QueryDatabase/PublisherConsole/Program.cs
+++ QueryDatabase/PublisherConsole/Program.cs
@@ -2,7 +2,45 @@
 using PublisherData;
 using PublisherDomain;
 
-GetAuthors();
+using PubContext _context = new();
+
+//GetAuthors();
+//QueryFilters();
+//FindIt();
+//AddSomeMoreAuthors();
+SkipAndTakeAuthors();
+
+void SkipAndTakeAuthors()
+{
+    var groupSize = 2;
+    for (var i = 0; i < 9; i++)
+    {
+        var authors = _context.Authors.Skip(i * groupSize).Take(groupSize).ToList();
+    }
+}
+
+void AddSomeMoreAuthors()
+{
+    _context.Authors.Add(new Author { FirstName = "Rastko", LastName = "Jovic"});
+    _context.Authors.Add(new Author { FirstName = "Dusan", LastName = "Kicanovic" });
+    _context.Authors.Add(new Author { FirstName = "Jovan", LastName = "Cvijic" });
+    _context.Authors.Add(new Author { FirstName = "Stefan", LastName = "Hvar" });
+
+    _context.SaveChanges();
+}
+
+void FindIt()
+{
+    var authorIdTwo = _context.Authors.Find(2);
+}
+
+void QueryFilters()
+{
+    //var firstname = "William";
+    //var authors = _context.Authors.Where(a => a.FirstName == firstname).ToList();
+    var filter = "K%";
+    var authors = _context.Authors.Where(a => EF.Functions.Like(a.LastName, filter)).ToList();
+}
 
 void GetAuthors()
 {
