2e372ceb494fb6df3ea0b0db805357dc2c0df55c
Nevena Kicanovic <nevena@contactus.se>
2025-02-25 10:46:23 +0100

    25 feb 10:46

---
+++ FirstApp/PublisherConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+//AddAuthor();
+GetAuthors();
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine($"{author.FirstName} {author.LastName}");
+    }
+
+}
+
+void AddAuthor()
+{
+    var author = new Author
+    {
+        FirstName = "Nevena",
+        LastName = "Kicanovic"
+    };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
---
+++ FirstApp/PublisherData/PubContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData
+{
+    public class PubContext : DbContext
+    {
+        public PubContext(){}
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        //Lägga till OnConfiguring () -metoden för att ange anslutningssträngen
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\mssqllocaldb;Database=PubDatabase;Trusted_Connection=True;");
+        }
+    }
+}
