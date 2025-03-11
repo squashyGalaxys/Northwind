59e360165e4c10c5c96729deb18c201663a305d5
Nevena Kicanovic <nevena@contactus.se>
2025-03-11 09:54:07 +0100

    enable static/default pages, razor page

--- PraktiskaAppar/Northwind.Web/Program.cs
+++ PraktiskaAppar/Northwind.Web/Program.cs
@@ -1,15 +1,25 @@
+#region  Konfigurera web server host och tjänster
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+
+
 var app = builder.Build();
+#endregion
 
 #region Konfiguration av HTTP pipeline och routing
-if(!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () => "Hello World!");
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapRazorPages();
+
+app.MapGet("/hello", () => $"Enviroment is {app.Environment.EnvironmentName}");
 #endregion
 
 app.Run();
