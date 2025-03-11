b3a3a8d27c813e9899f51e2e84d29b0954b574b3
Nevena Kicanovic <nevena@contactus.se>
2025-03-11 12:05:48 +0100

    POST - suppliers Orders - utan code-behind

--- PraktiskaAppar/Northwind.Web/Pages/Suppliers.cshtml.cs
+++ PraktiskaAppar/Northwind.Web/Pages/Suppliers.cshtml.cs
@@ -12,12 +12,31 @@ namespace Northwind.Web.Pages
             db = injectedContext;
         }
         public IEnumerable<Supplier>? Suppliers { get; set; }
+
+        [BindProperty]
+        public Supplier? Supplier { get; set; }
+
         public void OnGet()
         {
             ViewData["Title"] = "Northwind App - Suppliers";
 
             Suppliers = db.Suppliers.OrderBy(c => c.Country)
                 .ThenBy(c => c.CompanyName);
+        }
+
+        public IActionResult OnPost()
+        {
+            if ((Supplier is not null) && ModelState.IsValid)
+            {
+                db.Suppliers.Add(Supplier);
+                db.SaveChanges();
+                return RedirectToPage("/suppliers");
+            }
+            else
+            {
+                return Page(); //returnerar ursprungliga sidan
+            }
+
         }
     }
 }
