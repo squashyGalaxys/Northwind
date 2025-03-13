6b87377dde146b8c4cea97b00463ce845b1eb590
Nevena Kicanovic <nevena@contactus.se>
2025-03-13 09:30:36 +0100

    Razor klass bibliotek för employees

---
+++ PraktiskaAppar/Northwind.Razor.Employees/Areas/NorthwindFeatures/Pages/Employees.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Razor.Employees.MyFeature.Pages
+{
+    public class EmployeesModel : PageModel
+    {
+        public void OnGet()
+        {
+            ViewData["Title"] = "Northwind App - Employees";
+            //TODO: Implementera kod för att hämta 
+            //employees från databasen, sortera med LastName sen FirstName
+        }
+    }
+}
